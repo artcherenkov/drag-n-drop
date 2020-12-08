e1c757ecf7ad6f3dce4ab1cb92ad6a3a9b5968f3
Artem Cherenkov <art.cherenkov@gmail.com>
2020-12-09 02:17:34 +0700

    Рефакторинг модуля placeholder.js

--- src/utils/placeholder.js
+++ src/utils/placeholder.js
@@ -10,78 +10,46 @@ const getDraggedTask = draggableId => (
 );
 
 export const getPlaceholderBounds = (update) => {
+  const draggedDOM = getDraggedTask(update.draggableId);
+  if (!draggedDOM) {
+    return;
+  }
+  const { clientHeight, clientWidth } = draggedDOM;
+
   const destinationColumn = update.destination.droppableId;
   const sourceColumn = update.source.droppableId;
+  const sourceIndex = update.source.index;
+  const destinationIndex = update.destination.index;
+
+
+  const targetColumn = getColumn(destinationColumn)
+  let targetColumnTasks = [...targetColumn.children];
 
   if (destinationColumn === sourceColumn) {
-    const draggedDOM = getDraggedTask(update.draggableId);
-    if (!draggedDOM) {
-      return;
-    }
-    const { clientHeight, clientWidth } = draggedDOM;
-
-    const destinationIndex = update.destination.index;
-    const sourceIndex = update.source.index;
-
-
-    const targetColumn = getColumn(destinationColumn)
-
-    const targetColumnTasks = [...targetColumn.children];
     const movedItem = targetColumnTasks[sourceIndex];
     targetColumnTasks.splice(sourceIndex, 1);
 
-    const updatedArray = [
+    targetColumnTasks = [
       ...targetColumnTasks.slice(0, destinationIndex),
       movedItem,
       ...targetColumnTasks.slice(destinationIndex + 1)
     ];
-
-
-    const clientY =
-      parseFloat(window.getComputedStyle(targetColumn).paddingTop) +
-      updatedArray.slice(0, destinationIndex).reduce((total, curr) => {
-        const style = window.getComputedStyle(curr);
-        const marginBottom = parseFloat(style.marginBottom);
-        const borders = parseFloat(style.borderBottom) * 2;
-
-        return total + curr.clientHeight + marginBottom + borders;
-      }, 0);
-
-    return {
-      clientHeight,
-      clientWidth,
-      clientY,
-      clientX: parseFloat(window.getComputedStyle(targetColumn).paddingLeft)
-    }
-  }
-  else {
-    const draggedDOM = getDraggedTask(update.draggableId);
-    if (!draggedDOM) {
-      return;
-    }
-    const { clientHeight, clientWidth } = draggedDOM;
-
-    const destinationIndex = update.destination.index;
-    const targetColumn = getColumn(destinationColumn)
-    const targetColumnTasks = [...targetColumn.children];
-
-    const clientY =
-      parseFloat(window.getComputedStyle(targetColumn).paddingTop) +
-      targetColumnTasks.slice(0, destinationIndex).reduce((total, curr) => {
-        const style = window.getComputedStyle(curr);
-        const marginBottom = parseFloat(style.marginBottom);
-        const borders = parseFloat(style.borderBottom) * 2;
-
-        return total + curr.clientHeight + marginBottom + borders;
-      }, 0);
-
-    return {
-      clientHeight,
-      clientWidth,
-      clientY,
-      clientX: parseFloat(window.getComputedStyle(targetColumn).paddingLeft)
-    }
   }
 
+  const clientY =
+    parseFloat(window.getComputedStyle(targetColumn).paddingTop) +
+    targetColumnTasks.slice(0, destinationIndex).reduce((total, curr) => {
+      const style = window.getComputedStyle(curr);
+      const marginBottom = parseFloat(style.marginBottom);
+      const borders = parseFloat(style.borderBottom) * 2;
 
+      return total + curr.clientHeight + marginBottom + borders;
+    }, 0);
+
+  return {
+    clientHeight,
+    clientWidth,
+    clientY,
+    clientX: parseFloat(window.getComputedStyle(targetColumn).paddingLeft)
+  }
 }
